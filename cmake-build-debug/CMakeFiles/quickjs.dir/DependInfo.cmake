8cc23cfe7cf3b3f725f8a6497bfce4f885ca5053
LemonHX <runzhe2001@hotmail.com>
2020-07-20 23:34:33 +0200

    2020-07-05

---
+++ cmake-build-debug/CMakeFiles/quickjs.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/cutils.c" "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/cmake-build-debug/CMakeFiles/quickjs.dir/cutils.c.obj"
+  "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/libbf.c" "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/cmake-build-debug/CMakeFiles/quickjs.dir/libbf.c.obj"
+  "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/libregexp.c" "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/cmake-build-debug/CMakeFiles/quickjs.dir/libregexp.c.obj"
+  "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/libunicode.c" "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/cmake-build-debug/CMakeFiles/quickjs.dir/libunicode.c.obj"
+  "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/quickjs-libc.c" "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/cmake-build-debug/CMakeFiles/quickjs.dir/quickjs-libc.c.obj"
+  "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/quickjs.c" "C:/Users/Admin/Desktop/quickjs/quickjs-2020-07-05/cmake-build-debug/CMakeFiles/quickjs.dir/quickjs.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CONFIG_VERSION=\"2020-07-05\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/quickjs.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quickjs.dir/cutils.c.obj"
+  "CMakeFiles/quickjs.dir/libbf.c.obj"
+  "CMakeFiles/quickjs.dir/libregexp.c.obj"
+  "CMakeFiles/quickjs.dir/libunicode.c.obj"
+  "CMakeFiles/quickjs.dir/quickjs-libc.c.obj"
+  "CMakeFiles/quickjs.dir/quickjs.c.obj"
+  "quickjs.lib"
+  "quickjs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/quickjs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
